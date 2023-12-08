58e5bae92822e1cd5611868472e07f9715df7bfd
xdlimao <danieloliveiradesouza09@outlook.com>
2023-12-08 14:56:01 -0300

    preparando a tela de pedido para mandar ao finalizar que ira receber la por meio de sessions

--- pedido/index.php
+++ pedido/index.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    include("../static/php/mysqlconnection.php");
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AdmPage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../static/css/"> <!--utilizar o scss para deixar a nav bar fixa no lugar, https://www.w3schools.com/howto/howto_css_fixed_menu.asp-->
+    <title>Pedido</title>
 </head>
 <body>
-    
+    <nav class="navbar" id="navbar">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Produtos</span>
+            <div class="d-flex">
+                <a href="../finalizar/"><button type="button" class="btn btn-success">Ver carrinho</button></a>
+            </div>
+        </div>
+    </nav>
+
+    <div>
+    <?php
+        $sqllist1 = "SELECT * FROM produto ORDER BY name ASC";
+        $result = mysqli_query($conn, $sqllist1);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                //Nome e preço produto
+                echo "<p id='nameprodtxt'>" . $row["name"] . "</p>";
+                echo "<p id='priceprodtxt'>R$" . $row["price"] . "</p>";
+
+            }
+        }
+    ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php
-echo "oi";
-?>
